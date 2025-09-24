2dc3a221b2371c9d71f961c2b7c0baa263bad8f2
Andrew Edmonds <aedmonds@york.cuny.edu>
2025-09-24 11:22:05 -0500

    Remove old TrkQualTree scripts

--- scripts/TrkQualTree_v2_0.C
+++
@@ -1,8 +0,0 @@
-#include "TrkQualTree.hh"
-
-void TrkQualTree_v2_0() {
-  //  std::string input_filename = "filelists/nts.mu2e.CeEndpointMix1BBTriggered.MDC2020an_v06_01_01_best_v1_3.root.100-file-list";
-  std::string input_filename = "filelists/nts.mu2e.FlateMinusMix1BBTriggered.MDC2020aw_best_v1_3_v06_06_00.root.list";
-  std::string output_filename = "/exp/mu2e/data/users/edmonds/TrkQualTraining/trkqual_tree_v2.0.root";
-  TrkQualTree(input_filename, output_filename);
-}
